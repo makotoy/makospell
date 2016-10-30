fc3852a35f7a39df86bd72f338a8995a45e7ca79
Makoto Yamashita <makotoy@makobookproretina.local>
2016-10-30 20:30:02 +0900

    Initial Commit

---
+++ MakoSpellChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MakoSpellChecker
+//
+//  Created by Makoto Yamashita on ’16.10.30.
+//  Copyright © 2016 Makoto Yamashita. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MakoSpellChecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MakoSpellChecker
+//
+//  Created by Makoto Yamashita on ’16.10.30.
+//  Copyright © 2016 Makoto Yamashita. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
